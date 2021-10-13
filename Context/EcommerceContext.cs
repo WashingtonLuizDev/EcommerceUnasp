c57b5bf09680efa5010492e23fc3efd18d30a22e
Washington Luiz Santana de Moraes Silva <57022931+WashingtonLuizDev@users.noreply.github.com>
2021-10-13 12:26:21 -0300

    Inclusão das dependencias básicas do Entity Framework; Inclusão de classes básicas do projetos; Inclusão do Context do Entity FrameWork;

---
+++ Context/EcommerceContext.cs
@@ -0,0 +1,27 @@
+﻿using EcommerceUnasp.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceUnasp.Context
+{
+    public class EcommerceContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=45.93.100.120,1433;initial catalog=FS01;persist security info=True;user id=FS01;password=25846;MultipleActiveResultSets=True;App=exeEf;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
