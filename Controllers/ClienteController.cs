7c333bcd98d2f21d8798100f92f111aab9f3077d
Washington Luiz Santana de Moraes Silva <57022931+WashingtonLuizDev@users.noreply.github.com>
2021-10-14 22:16:37 -0300

    Inclusão do primeiro controller de Cliente
    Inclusão do primeiro controller de clientes;
    Inclusão do mapeamento das tabelas como DbSet's no contexto;

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,81 @@
+﻿using EcommerceUnasp.Context;
+using EcommerceUnasp.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceUnasp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        private EcommerceContext _context = new EcommerceContext();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Cliente>> Get()
+        {
+            using (_context)
+            {
+                return Ok(_context.Clientes.ToList());
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Cliente> GetById(int id)
+        {
+            using (_context)
+            {
+                Cliente user = _context.Clientes.Where(u => u.Id.Equals(id)).FirstOrDefault();
+
+                if (user == null)
+                    return NotFound();
+
+                return user;
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<Cliente> Post(Cliente cliente)
+        {
+            using (_context)
+            {
+                _context.Clientes.Add(cliente);
+                _context.SaveChanges();
+            }
+            return cliente;
+        }
+
+        [HttpPut]
+        public ActionResult<Cliente> Put(Cliente cliente)
+        {
+            using (_context)
+            {
+                _context.Clientes.Update(cliente);
+                _context.SaveChanges();
+            }
+
+            return cliente;
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            using (_context)
+            {
+                Cliente cliente = _context.Clientes.Where(u => u.Id.Equals(id)).FirstOrDefault();
+
+               if (cliente == null)
+                    return NotFound();
+
+                _context.Clientes.Remove(cliente);
+                _context.SaveChanges();
+            }
+
+            return Ok();
+        }
+    }
+}
