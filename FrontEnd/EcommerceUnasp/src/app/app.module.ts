9be704c59ce894bf8fa15b43c82ca132c45cff56
Washington Luiz Santana de Moraes Silva <57022931+WashingtonLuizDev@users.noreply.github.com>
2021-11-04 22:56:37 -0300

    Inclusão do Cors no backend. Inclusão do form de cadastro de produto; Inclusão do método de Salvar produto;

--- FrontEnd/EcommerceUnasp/src/app/app.module.ts
+++ FrontEnd/EcommerceUnasp/src/app/app.module.ts
@@ -3,18 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    ProdutosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ FrontEnd/EcommerceUnasp/src/app/components/produtos/produtos.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Produto } from 'src/app/models/produto.model';
+import { ProdutosService } from 'src/app/services/produtos.service';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.css']
+})
+export class ProdutosComponent implements OnInit {
+
+  produto : Produto = new Produto();
+  form: FormGroup;
+  imagemFormControl = this.formBuild.control('', {
+    validators: [], updateOn: 'blur'
+  })
+  tituloFormControl = this.formBuild.control('', {
+    validators: [Validators.required], updateOn: 'blur'
+  });
+  categoriaFormControl = this.formBuild.control('', {
+    validators: [], updateOn: 'blur'
+  });
+  descricaoFormControl = this.formBuild.control('', {
+    validators: [Validators.required], updateOn: 'blur'
+  });
+  valorFormControl = this.formBuild.control('', {
+    validators: [], updateOn: 'blur'
+  });
+
+  constructor(private formBuild: FormBuilder,
+    private produtoService: ProdutosService) {
+    this.form = this.formBuild.group({
+      imagemFormControl: this.imagemFormControl,
+      tituloFormControl: this.tituloFormControl,
+      valorFormControl: this.valorFormControl,
+      descricaoFormControl: this.descricaoFormControl,
+      categoriaFormControl: this.categoriaFormControl
+    });
+  }
+
+
+  ngOnInit(): void {
+  }
+
+  salvar() {
+    //Somente é verdade se todos os Validators forem atendidos
+    if (this.form.valid) {
+      //Toda variavel não inicializado ou é undefined ou null
+      if (this.produto == undefined || this.produto == null)
+        this.produto = new Produto(); //Instanciando um Objeto
+
+      this.produto.titulo = this.tituloFormControl.value;
+      this.produto.descricao = this.descricaoFormControl.value;
+      this.produto.imagem = this.imagemFormControl.value;
+      this.produto.valor = this.valorFormControl.value;
+      this.produto.categoriaId = this.categoriaFormControl.value;
+
+      //Se a ordem de serviço ainda não existe, então não temos o ID
+      if (this.produto.id == undefined || this.produto.id == null) {
+        //Caso não tenha ID, vamos inserir atraves do Post
+        this.produtoService.post(this.produto).subscribe((produto: Produto) => {
+          this.produto = produto; //Variavel do tipo objeto lá de cima receberá o objeto do backend
+
+        });
+      } else {
+        //Caso já tenhamos ID, atualizaremos atraves do Put
+        this.produtoService.put(this.produto).subscribe((produto: Produto) => {
+          this.produto = produto; //Variavel do tipo objeto lá de cima receberá o objeto do backend
+        });
+      }
+    }
+  }
+}
